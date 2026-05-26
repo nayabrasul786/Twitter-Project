b263d7673bf244cfb3c71b4b80b022523a301969
nayabrasul786 <shaiknayabrasul09@gmail.com>
2026-05-26 11:19:58 +0000

    fixed vercel deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
 const path = require('path')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const app = express()
